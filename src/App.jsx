271c5240bff4b6dc12de3f0603821d39351c175a
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-01-20 11:28:40 +0200

    Add css styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import CardImage from './components/CardImage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='text-3xl text-red-800'>Hello</h1>
-    </>
+    <div className='bg-Graysmoke'>
+      <CardImage/>
+    </div>
   )
 }
 
--- src/components/CardImage.jsx
+++ src/components/CardImage.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Union from '../img/Union.png'
 
 const CardImage = () => {
   return (
-    <div className='bg-white'>
+    <div className="bg-white bg-[url('../img/Union.png')]">
         <section>
+            <nav>
+                <p>Bunbee</p>
+                <a href="#"><img src={Union} alt="" /></a>
+                <ul>
+                    <li>Portfolio</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
 
         </section>
 
