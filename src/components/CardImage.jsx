d195db3394b3940da7985f8e8a60b768f0fa7396
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-01-20 09:49:03 +0200

    Add tailwind class

---
+++ src/components/CardImage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CardImage = () => {
+  return (
+    <div className='bg-white'>
+        <section>
+
+        </section>
+
+    </div>
+  )
+}
+
+export default CardImage
