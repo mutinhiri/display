f7b12afe5ac1ab23c32869e88e5d347553c438c3
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-01-20 12:17:42 +0200

    Add list Items

--- src/components/CardImage.jsx
+++ src/components/CardImage.jsx
@@ -9,10 +9,10 @@ const CardImage = () => {
             <nav className='grid grid-cols-2'>
                 <p className='text-blue-500 capitalize font-bold text-xl '>Bunbee</p>
                 {/* <a href="#"><img src={Heading} alt="" /></a> */}
-                <ul className='grid grid-cols-3 ml-52'>
-                    <li>Portfolio</li>
-                    <li>About</li>
-                    <li>Contact</li>
+                <ul className='grid grid-cols-6 ml-52 gap-2 mt-2'>
+                    <li className='col-start-2'>Portfolio</li>
+                    <li className=''>About</li>
+                    <li className=''>Contact</li>
                 </ul>
             </nav>
 
